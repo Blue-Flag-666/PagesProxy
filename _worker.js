c19c3e1b8c008a82f0ec5445b9898ca071b85160
Galaxy-Studio-Code <64064866+Galaxy-Studio-Code@users.noreply.github.com>
2022-05-24 18:17:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'google.com'
+      url.hostname = '43.142.12.216:8888'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
