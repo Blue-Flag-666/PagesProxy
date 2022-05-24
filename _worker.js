0d2e662dc0123c86e05a4aa272e175da8e6a14d8
Galaxy-Studio-Code <64064866+Galaxy-Studio-Code@users.noreply.github.com>
2022-05-24 18:19:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '43.142.12.216:8888'
+      url.hostname = 'baidu.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
